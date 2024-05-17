3b1279b25bb011539c8377daaffb4914503b69c3
CNBarrhaven <148283318+CNBarrhaven@users.noreply.github.com>
2024-05-17 19:05:06 -0400

    data management
    its not fun, crois-moi

---
+++ Assets/Scripts/dayScript.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Order
+{
+    public float appearTime;
+
+    public bool isSpawned;
+
+    public bool needsCutTomato;
+    public bool needsCutMelon;
+    public bool needsPizza;
+    public bool needsCandyCane;
+    public bool needsKetchup;
+    public bool needsSalmon;
+    public bool needsLollipop;
+
+    public Order(float appearTime, bool isSpawned, bool needsCutTomato, bool needsCutMelon, bool needsPizza, bool needsCandyCane, bool needsKetchup, bool needsSalmon, bool needsLollipop)
+    {
+        this.appearTime = appearTime;
+        this.isSpawned = isSpawned;
+
+        this.needsCutTomato = needsCutTomato;
+        this.needsCutMelon = needsCutMelon;
+        this.needsPizza = needsPizza;
+        this.needsCandyCane = needsCandyCane;
+        this.needsKetchup = needsKetchup;
+        this.needsSalmon = needsSalmon;
+        this.needsLollipop = needsLollipop;
+    }
+}
+
+public class dayScript : MonoBehaviour
+{
+
+    public List<Order> orders = new List<Order>();
+
+    public float timer;
+
+    public GameObject orderSlot1;
+    public bool slot1Used;
+    public GameObject orderSlot2;
+    public bool slot2Used;
+    public GameObject orderSlot3;
+    public bool slot3Used;
+
+    public orderManager orderManager;
+    private void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        foreach (Order order in orders)
+        {
+            if (timer >= order.appearTime && !order.isSpawned)
+            {
+                orderManager.spawnOrder(order);
+            }
+        }
+    }
+}
--- Assets/Scripts/orderManager.cs
+++ Assets/Scripts/orderManager.cs
@@ -7,6 +7,14 @@ public class orderManager : MonoBehaviour
 {
     public float timer;
     public int day;
+
+    public bool slot1used;
+    public bool slot2used;
+    public bool slot3used;
+
+    public GameObject orderObject1;
+    public GameObject orderObject2;
+    public GameObject orderObject3;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +24,48 @@ public class orderManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        if (day == 1)
+
+    }
+
+    public void spawnOrder(Order order)
+    {
+        if (!slot1used)
         {
-            if (timer >= 10)
+            orderObject1.SetActive(true);
+            if (order.needsCutTomato)
             {
-
+                orderObject1.transform.GetChild(1).gameObject.SetActive(true);
             }
-        }
+            if (order.needsCutMelon)
+            {
+                orderObject1.transform.GetChild(2).gameObject.SetActive(true);
+            }
+            if (order.needsPizza)
+            {
+                orderObject1.transform.GetChild(3).gameObject.SetActive(true);
+            }
 
+            slot1used = true;
+        }
+        else if (!slot2used)
+        {
+            slot2used = true;
+        }
+        else if (!slot3used)
+        {
+            slot3used = true;
+        }
+        else
+        {
+            
+        }
+        // you need some kind of public variable for which order slots are full
+        // when an order is spawned in one it becomes true, order finished it becomes false
+        // maybe give order a int slot variable or something
+
+        // if there is an empty slot then
+        // set orderObject at that slot to active
+        // set/unset appropriate sprites
+        // change order's slot variable?
     }
 }
